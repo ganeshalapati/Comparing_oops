2e1be1a28731ede687b11e62864118c472733c9a
ganeshalapati <alapatiganesh31@gmail.com>
2021-05-17 18:09:23 +0530

    added length of two lines

---
+++ Length.java
@@ -0,0 +1,19 @@
+public class Length {
+	static int x1=1;
+	static int x2=2;
+	static int y1=3;
+	static int y2=4;
+	   
+	   public static void main(String[] args) {
+
+		double length = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+		
+		System.out.println("Length of two lines is " + length);
+		
+
+ 
+		
+		
+
+	}
+}
